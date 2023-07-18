8c6c040ed179dfa4cb1f4a517097ca591d9efc18
Pedro Pereira <77394454+pedropereirap-ms@users.noreply.github.com>
2023-07-18 11:14:44 +0000

    Copilot third2 commit

--- comments.js
+++ comments.js
@@ -1,29 +1,16 @@
 // create web server
-// create web server
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
-const cors = require('cors');
-const port = 4001;
-const comments = require('./comments.json');
-const fs = require('fs');
 
-app.use(cors());
+// parse incoming requests with JSON payloads
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-app.get('/comments', (req, res) => {
-  res.json(comments);
-});
+// enable CORS
+const cors = require('cors');
+app.use(cors());
 
-app.post('/comments', (req, res) => {
-  const newComment = req.body;
-  newComment.id = comments.length + 1;
-  comments.push(newComment);
-  fs.writeFile('./comments.json', JSON.stringify(comments), () => {
-    res.json(comments);
-  });
-});
+// connect to database
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/comments', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
