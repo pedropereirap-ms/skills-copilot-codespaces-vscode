97ece7e4970a52874a3073de7d329d056154b86d
Pedro Pereira <77394454+pedropereirap-ms@users.noreply.github.com>
2023-07-18 11:16:06 +0000

    Copilot third3 commit

--- comments.js
+++ comments.js
@@ -1,16 +1,34 @@
 // create web server
-const express = require('express');
-const app = express();
+// run node comments.js
+// open browser and visit
+// http://localhost:3000/comments
+// http://localhost:3000/comments/new
 
-// parse incoming requests with JSON payloads
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
+var http = require('http');
+var fs = require('fs');
+var qs = require('querystring');
+var comments = [];
 
-// enable CORS
-const cors = require('cors');
-app.use(cors());
-
-// connect to database
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/comments', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
-
+http.createServer(function(req, res) {
+  if (req.url === '/' && req.method === 'GET') {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(fs.readFileSync('./comments.html'));
+  } else if (req.url === '/comments' && req.method === 'GET') {
+    var data = JSON.stringify(comments);
+    res.writeHead(200, {'Content-Type': 'application/json', 'Content-Length': data.length});
+    res.end(data);
+  } else if (req.url === '/comments' && req.method === 'POST') {
+    var body = '';
+    req.on('data', function(chunk) {
+      body += chunk;
+    });
+    req.on('end', function() {
+      var comment = qs.parse(body);
+      comments.push(comment);
+      res.end('success');
+    });
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
+}).listen(3000);
